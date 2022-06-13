a6f5c61fecc3485d149042be83546307c0042802
andreanadi <andreanadi18@gmail.com>
2022-06-13 15:37:44 +0700

    menambahkan nama

---
+++ phpdasar/pertemuan2/index.php
@@ -0,0 +1,52 @@
+<?php 
+//Pertemuan 2 - PHP Dasar
+//Sintaks PHP
+
+//Standar Output
+//echo/print
+//print_r
+//var_dump
+
+// echo "Andrean Adi Pratama";
+
+//Penulisan sintaks PHP
+//1. PHP di dalam HTML
+//2. HTML di dalam
+
+//Variabel dan Tipe Data
+//varibel
+//tidak boleh diawali dengan angka,tapi boleh mengandung angka
+// $nama = "Andrean Adi";
+// echo "Halo, nama saya $nama"
+
+//Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+//penggabung string/concatenation/concat
+// .
+$nama_depan = "Sandhika";
+$nama_belakang = "Galih";
+echo $nama_depan ." ". $nama_belakang
+
+?>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Belajar PHP</title>
+</head>
+<body>
+
+<h1>Halo, Selamat datang <?php echo $nama;?></h1>
+<p><?php echo "ini adalah paragraf"?></p>
+
+</body>
+</html> -->
+
+
---
+++ webprogramming/test/webprograming.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Web Programming</title>
+</head>
+<body>
+<p> Andrean Adi Pratama  </p>  
+
+</body>
+</html>
